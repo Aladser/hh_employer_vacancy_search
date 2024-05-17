39b4f3eba2ac93eef8b6cbf5271e91894d12de12
Aladser <aladser@gmail.com>
2024-05-17 12:45:06 +0900

    заполнение ДБ таблицы вакансий

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import psycopg2
 import requests
 
-
 SCHEMA_NAME = 'public'
 CONFIG_FILENAME = 'env'
 EMPLOYERS_FILENAME = 'employers'
@@ -34,52 +33,99 @@ with open(EMPLOYERS_FILENAME, 'r') as file:
         name = name.replace('\n', '')
         employers_list.append({'id': int(id), 'name': name})
 
-
 if __name__ == '__main__':
     conn = psycopg2.connect(**conn_params)
     cursor = conn.cursor()
 
-    """
     # -----Создание таблиц-----
     cursor.execute(f"create schema if not exists {SCHEMA_NAME};")
-    cursor.execute(f"DROP TABLE IF EXISTS {SCHEMA_NAME}.employers")
     cursor.execute(f"DROP TABLE IF EXISTS {SCHEMA_NAME}.vacancies")
-    # -----работодатели-----
+    cursor.execute(f"DROP TABLE IF EXISTS {SCHEMA_NAME}.employers")
+
+    # -----Работодатели-----
     cursor.execute(f"create table {SCHEMA_NAME}.employers("
                    f"employer_id bigint primary key,"
                    f"employer_name varchar(255) not null"
                    f")")
-    # -----вакансии-----
+    # -----Вакансии-----
     cursor.execute(f"create table {SCHEMA_NAME}.vacancies("
-                   f"worker_id bigint primary key,"
-                   f"worker_name varchar(255) not null,"
-                   f"worker_salary_from integer,"
-                   f"worker_salary_to integer,"
-                   f"worker_salary_currency varchar(10),"
+                   f"vacancy_id bigint primary key,"
+                   f"vacancy_name varchar(255) not null,"
+                   f"vacancy_url varchar(255),"
+                   f"vacancy_salary_from integer,"
+                   f"vacancy_salary_to integer,"
+                   f"vacancy_salary_currency varchar(10),"
                    f"employer_id bigint references {SCHEMA_NAME}.employers(employer_id)"
                    f")")
-    """
-
     conn.commit()
-    cursor.close()
-    conn.close()
+
+    # -----заполнение таблицы Работодатели-----
+    for employer in employers_list:
+        query = (f"insert into {SCHEMA_NAME}.employers (employer_id, employer_name) "
+                 f"values({employer['id']}, '{employer['name']}') returning *")
+        cursor.execute(query)
+    conn.commit()
 
     # -----запрос вакансий на api.hh.ru-----
     vacancies_list = []
     REQUEST_PARAMS['text'] = ''
+
     # запрос вакансий
     for employer in employers_list:
         REQUEST_PARAMS['page'] = 0
         REQUEST_PARAMS['employer_id'] = employer['id']
-        while REQUEST_PARAMS['page'] != REQUEST_PAGE_COUNT :
+        while REQUEST_PARAMS['page'] != REQUEST_PAGE_COUNT:
             response = requests.get(REQUEST_URL, headers=REQUEST_HEADERS, params=REQUEST_PARAMS)
             resp_vacancies = response.json()['items']
             vacancies_list.extend(resp_vacancies)
             REQUEST_PARAMS['page'] += 1
-    # удаление вакансий без ЗП
+
+    # добавление вакансии в БД
     for vcn in vacancies_list:
+        # пропуск вакансий без указанной зарплаты
         if vcn['salary'] is None:
-            vacancies_list.remove(vcn)
-    [print(vcn) for vcn in vacancies_list]
+            continue
 
+        # объект вакансии
+        db_vacancy = {
+            'id': int(vcn['id']),
+            'name': vcn['name'],
+            'salary_from': vcn['salary']['from'],
+            'salary_to': vcn['salary']['to'],
+            'salary_currency': vcn['salary']['currency'],
+            'employer_id': int(vcn['employer']['id']),
+            'url': vcn['alternate_url']}
 
+        # добавление в БД
+        query = (f"insert into {SCHEMA_NAME}.vacancies ("
+                 f"vacancy_id, vacancy_name, "
+                 f"employer_id, "
+                 f"vacancy_url, "
+                 f"vacancy_salary_currency,")
+
+        if db_vacancy['salary_from'] and db_vacancy['salary_to']:
+            query += 'vacancy_salary_from, vacancy_salary_to '
+        elif db_vacancy['salary_from']:
+            query += 'vacancy_salary_from '
+        else:
+            query += 'vacancy_salary_to '
+
+        query += (f") values ("
+                  f"{db_vacancy['id']}, '{db_vacancy['name']}', "
+                  f"{db_vacancy['employer_id']}, "
+                  f"'{db_vacancy['url']}', "
+                  f"'{db_vacancy['salary_currency']}', ")
+
+        if db_vacancy['salary_from'] and db_vacancy['salary_to']:
+            query += f"{db_vacancy['salary_from']}, {db_vacancy['salary_to']}"
+        elif db_vacancy['salary_from']:
+            query += f"{db_vacancy['salary_from']}"
+        else:
+            query += f"{db_vacancy['salary_to']}"
+
+        query += ") returning *"
+        cursor.execute(query)
+
+    conn.commit()
+    cursor.close()
+    conn.close()
